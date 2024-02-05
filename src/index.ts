0cb7200e47d2930033c37ca74a98ce13b1dfb000
Su386 <83100266+Su386yt@users.noreply.github.com>
2024-02-04 18:16:04 -0800

    Moved it to userManagement file

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,11 @@
 import { PrismaClient } from '@prisma/client'
 import { v4 as uuidv4 } from 'uuid';
+import { getUser, getUserUUID } from './userManagement';
 
 
 
 
-const prisma = new PrismaClient()
+export const prisma = new PrismaClient()
 
 const userId = uuidv4()
 const username = "flagmaster"
@@ -22,43 +23,6 @@ async function main() {
     console.log("Printing user data")
     const queriedUser = await getUser(currentUserId)
     console.log(queriedUser)
-}
-
-async function createNewUser(userId: string, username: string, displayName: string) {
-    const user = await prisma.user.create({
-        data: {
-            userId: userId,
-            username: username,
-            displayName: displayName
-        },
-    })
-    prisma.$disconnect()
-
-    return user
-}
-
-async function getUser(userId: string) {
-    const user = await prisma.user.findUnique({
-        where: {userId: userId}
-    })
-    prisma.$disconnect()
-
-    return user
-}
-
-async function getUserUUID(username: string) {
-    const data = await prisma.user.findUnique(
-        {
-            where: {username: username},
-            select: {userId: true}
-        }
-    )
-
-    if (data?.userId != null) {
-        return data.userId
-    } else {
-        return ""
-    }
 }
 
 main();
